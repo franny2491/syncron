6a9dfc5a9ff768724d7475854a470ad60c92c658
Armadillo <franny2491@gmail.com>
2015-11-16 23:01:59 +0000

    Push Notifications: Tutorial 1

--- Syncron/AppDelegate.swift
+++ Syncron/AppDelegate.swift
@@ -15,32 +15,53 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        registerNotificationActions()
+        
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        let notification :UILocalNotification = UILocalNotification()
+        notification.fireDate = NSDate(timeIntervalSinceNow: 10)
+        notification.alertBody = "Notification Fired"
+        notification.category = "custom_category_id"
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    }
+    
+    func application(application: UIApplication, handleActionWithIdentifier identifier: String?, forLocalNotification notification: UILocalNotification, completionHandler: () -> Void) {
+        
+        if notification.category == "custom_category_id" {
+            if identifier == "cancel_action_id" {
+                print("Cancel was pressed")
+            }
+            else if identifier == "reply_action_id" {
+                print("Reply was pressed")
+            }
+        }
+        
+        completionHandler()
     }
 
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    func registerNotificationActions() {
+        let cancelAction :UIMutableUserNotificationAction = UIMutableUserNotificationAction()
+        cancelAction.identifier = "cancel_action_id"
+        cancelAction.title = "Cancel"
+        cancelAction.activationMode = UIUserNotificationActivationMode.Background
+        cancelAction.destructive = true
+        
+        let replyAction :UIMutableUserNotificationAction = UIMutableUserNotificationAction()
+        replyAction.identifier = "reply_action_id"
+        replyAction.title = "Reply"
+        replyAction.activationMode = UIUserNotificationActivationMode.Foreground
+        replyAction.destructive = false
+        
+        let category :UIMutableUserNotificationCategory = UIMutableUserNotificationCategory()
+        category.identifier = "custom_category_id"
+        category.setActions([cancelAction, replyAction], forContext: UIUserNotificationActionContext.Default)
+  
+        let categories :NSSet = NSSet(array: [category])
+        let settings :UIUserNotificationSettings = UIUserNotificationSettings(forTypes: [UIUserNotificationType.Badge, UIUserNotificationType.Sound, UIUserNotificationType.Alert], categories: (categories as? Set<UIUserNotificationCategory>))
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
     }
-
-
 }
 
